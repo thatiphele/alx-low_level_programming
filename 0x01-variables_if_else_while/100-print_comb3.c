99fb86f6ac94dfa3cbcf8208e541cf8088952f29
thatiphele <thatayaonephele@gmail.com>
2023-02-18 02:38:12 -0800

    Print Com3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,8 @@
  */
 int main(void)
 {
-	int digit1 = 0, digit2 = 0;	
+	int digit1 = 0, digit2 = 0;
+
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
