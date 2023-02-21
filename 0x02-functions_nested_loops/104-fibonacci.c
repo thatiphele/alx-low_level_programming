4618753771766357bfc46d6e3f929563445ad76a
thatiphele <thatayaonephele@gmail.com>
2023-02-21 11:46:56 -0800

    Find and print 1st 98 Fibonacci numbers,start val 1 &2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,64 +1,42 @@
 #include <stdio.h>
-
-
 /**
- * numLength - returns the lenth of string
- * @num : operand number
- * Return: number of digits
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: ALways 0 (Success)
  */
-
-int numLength(int num)
-{
-	int length = 0;
-
-	if (!num)
-	{
-		return (1);
-	}
-
-	while (num)
-	{
-		num = num / 10;
-		length ++1;
-	}
-
-	return (length);
-}
-/**
- *  *main - prints the first 98 fibonaci sequences
- *  Return: 0
- */
-
 int main(void)
 {
-	unsigned long F_1 = 1, F_2 = 2, m_x = 100000000, tmp;
-	unsigned long f_1o = 0, f_2o = 0, tmp_o = 0;
-	short int k = 1, initial_10s;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	while (k <= 98)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		if (F_1o > 0)
-			printf("%lu", f_1o);
-		initial_l0s = numLength(m_x) - 1 - numLength(F_1);
-		while (F_1o > 0 && initial_10s > 0)
-		{
-			printf("%i", 0);
-			initial_l0s--;
-		}
-		printf("%lu", f1);
-
-		tmp = (F_1 + F_2) % m_x;
-		tmp_o = f_1o + f_2o + (F_1 + F_2) / m_x;
-		F_1 = F_2;
-		F_1o = F_2o;
-		F_2 = tmp;
-		F_2o = tmp_o;
-
-		if (k != 98)
-			printf(", ");
-		else
-			printf("\n");
-		k++;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
+
 	return (0);
 }
