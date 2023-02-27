25a5e6a3a268e07ba21f3fcb0eecd8069b424c50
thatiphele <thatayaonephele@gmail.com>
2023-02-27 08:08:12 -0800

    A function that prints half of a string, followed by a new line.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  *puts2 - prints even characters of a string
  *
@@ -9,14 +11,31 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
+	int a;
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 1)
+{
+a = (i + 1) / 2;
+while (str[a] != '\0')
+{
+_putchar(str[a]);
+a++;
+}
+}
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-	}
-	_putchar('\n');
+else
+{
+a = (i / 2);
+while (str[a] != '\0')
+{
+_putchar(str[a]);
+a++;
+}
+}
+
+_putchar('\n');
 }
