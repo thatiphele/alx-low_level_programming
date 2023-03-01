dd7343a821fd141f7963e72bffc977a9634d4fd0
thatiphele <thatayaonephele@gmail.com>
2023-03-01 09:06:40 -0800

    Compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * _strcmp - compare 2 strings
  *@s1: 1st string
  *@s2: 2nd string
  * Return: 0 if match, and ns1 - ns2 if no match
  */
+int _strcmp(char *s1, char *s2)
 {
 	while (*s && *s2 && *s1 == *s2)
 	{
