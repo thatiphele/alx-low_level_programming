92f88a50499cbd15c7ca200e7b43d754cdfacca6
thatiphele <thatayaonephele@gmail.com>
2023-03-01 09:11:12 -0800

    Hot Fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcmp - compare 2 strings
@@ -7,7 +6,9 @@
  *@s2: 2nd string
  * Return: 0 if match, and ns1 - ns2 if no match
  */
+
 int _strcmp(char *s1, char *s2)
+
 {
 	while (*s && *s2 && *s1 == *s2)
 	{
