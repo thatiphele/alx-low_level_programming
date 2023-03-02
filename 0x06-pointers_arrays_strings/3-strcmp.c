5a393996f9aba1ad3b34bdebbb9b02a600301b35
thatiphele <thatayaonephele@gmail.com>
2023-03-02 00:26:03 -0800

    Hot Fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,13 +8,16 @@
  */
 
 int _strcmp(char *s1, char *s2)
-
 {
-	while (*s && *s2 && *s1 == *s2)
-	{
-	s1++;
-	s2++;
-	}
-	return (*s1 - *s2);
+	int x = 0;
 
+	while (s1[x] != '\0' && s2[x] != '\0')
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+		x++;
+	}
+	return (0);
 }
