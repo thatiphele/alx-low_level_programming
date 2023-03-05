49cdb2b61631102c85ff9f820bdce3a5b44e13c0
thatiphele <thatayaonephele@gmail.com>
2023-03-05 05:21:07 -0800

    Hot Fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,26 +2,21 @@
 
 /**
  *puts_half - prints half the characters of a string
- *
  *@str:input
- *
  *Return:void
- *
  */
 
 void puts_half(char *str)
 {
-	int count, string = 0;
+	int x = 0;
 
-	while (str[string] != '\0')
-		string++;
-	if (string + 1 % 2 != '0')
-		count = (string - 1) / 2;
-	else
-		count = (string / 2);
-	count++;
-
-for (string = count; str[string] != '\0'; string++)
-	_putchar(str[string]);
+	for (x = 0; str[x] != '\0'; x++)
+	{
+	}
+	x++;
+	for (x = x / 2; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
 	_putchar('\n');
 }
